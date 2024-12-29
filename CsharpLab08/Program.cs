8fd64ea15e87ca3fbe3825ed17c11ebfd2a15020
Titanoboba <79159681+Titanoboba@users.noreply.github.com>
2024-12-29 22:22:07 +0300

    Update Program.cs

--- CsharpLab08/Program.cs
+++ CsharpLab08/Program.cs
@@ -13,7 +13,7 @@ class Programm
         XmlSerializer xmlSerializer = new XmlSerializer(typeof(Lion));
 
         // десериализуем объект
-        using (FileStream fs = new FileStream("C:\\Users\\Python\\source\\repos\\CsharpLab08\\CharpLab08Program\\bin\\Debug\\net8.0\\animal.xml", FileMode.OpenOrCreate))
+        using (FileStream fs = new FileStream("animal.xml", FileMode.OpenOrCreate))
         {
             Lion? _lion = xmlSerializer.Deserialize(fs) as Lion;
             Console.WriteLine($"Name: {_lion?.Name}  Country: {_lion?.Country} Hide: {_lion?.HideFromOtherAnimals} WhatAnimeal: Animal : {_lion?.WhatAnimal}");
